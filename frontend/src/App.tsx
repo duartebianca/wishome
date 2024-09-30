45609d38d396d24f3bda6273dd1cc40fdf13bdac
duartebianca <biancaduarte1914@gmail.com>
2024-09-30 07:06:03 -0300

    feat(routes): add login, register, password recovery, and password reset pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./app/home/homePage";
 import NotFound from "./app/notFound/notFound";
+import HomePage from "./app/home/homePage";
+import LoginPage from "./app/login/login";
+import SignUpPage from "./app/register/register";
+import GiftListPage from "./app/list/list";
+import PasswordRecoveryPage from "./app/password/recover/recoverPassword";
+import ResetPasswordPage from "./app/password/reset/resetPassword";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +15,26 @@ const router = createBrowserRouter([
   {
     path: "*",
     element: <NotFound />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <SignUpPage />,
+  },
+  {
+    path: "/password-recovery",
+    element: <PasswordRecoveryPage />,
+  },
+  {
+    path: "/password-reset",
+    element: <ResetPasswordPage />,
+  },
+  {
+    path: "/list",
+    element: <GiftListPage />,
   },
 ]);
 
