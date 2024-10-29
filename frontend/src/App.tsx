1bd38438778a27c1d6b57a5477243443f3ce5ee4
duartebianca <biancaduarte1914@gmail.com>
2024-10-29 16:12:57 -0300

    refactor: Remove unnecessary import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
 import NotFound from "./app/notFound/notFound";
 import HomePage from "./app/home/homePage";
 import SignUpPage from "./app/register/register";
