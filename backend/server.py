88f1dd1242d159de9cd27fbb86c2c9b995fb0a71
duartebianca <biancaduarte1914@gmail.com>
2024-11-04 03:18:02 -0300

    refactor: add JWT token expiration configuration to enhance security

--- backend/server.py
+++ backend/server.py
@@ -5,7 +5,7 @@ from flask_jwt_extended import JWTManager, create_access_token, jwt_required, ge
 from flask_cors import CORS
 import secrets
 from email_service.email_service import EmailService
-
+from datetime import timedelta
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
@@ -13,6 +13,8 @@ CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = secrets.token_hex(32)
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=180)
+
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
