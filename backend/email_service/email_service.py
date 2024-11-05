629d2ee4041aede98cbf33fb458767b441fd34d1
duartebianca <biancaduarte1914@gmail.com>
2024-11-04 23:41:04 -0300

    feat: enhance email service to support dynamic templates and add tracking code HTML

--- backend/email_service/email_service.py
+++ backend/email_service/email_service.py
@@ -6,14 +6,11 @@ from dotenv import load_dotenv
 
 class EmailService:
     @staticmethod
-    def send_email(to: str, subject: str) -> None:
-        print(to)
-        # Obtém as credenciais do e-mail do arquivo .env
+    def send_email(to: str, subject: str, template: str = "email.html", context: dict = None) -> None:
         load_dotenv()
         USER_EMAIL = os.getenv("USER_EMAIL")
         USER_PASSWORD = os.getenv("USER_PASSWORD")
 
-        # Garante que USER_EMAIL e USER_PASSWORD são strings
         if isinstance(USER_EMAIL, bytes):
             USER_EMAIL = USER_EMAIL.decode("utf-8")
         if isinstance(USER_PASSWORD, bytes):
@@ -22,11 +19,16 @@ class EmailService:
         SMTP_SERVER = "smtp.gmail.com"
         SMTP_PORT = 587
 
-        # Carrega o conteúdo HTML do e-mail
+        # Carrega o conteúdo HTML do template
         current_dir = os.path.dirname(os.path.abspath(__file__))
-        email_template_path = os.path.join(current_dir, "email.html")
+        email_template_path = os.path.join(current_dir, template)
         with open(email_template_path, "r", encoding="utf-8") as file:
             html_content = file.read()
+
+        # Insere o conteúdo dinâmico, se houver (ex: código de rastreio)
+        if context:
+            for key, value in context.items():
+                html_content = html_content.replace(f"{{{{{key}}}}}", value)
 
         # Configura a mensagem de e-mail
         msg = MIMEMultipart()
@@ -40,7 +42,7 @@ class EmailService:
         # Envia o e-mail
         try:
             with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
-                server.starttls()  # Inicializa a comunicação segura
+                server.starttls()
                 server.login(USER_EMAIL, USER_PASSWORD)
                 server.send_message(msg)
             print("E-mail enviado com sucesso")
