06184b7a1136da80cdf401fcad1345f42bacd3cb
duartebianca <biancaduarte1914@gmail.com>
2024-11-06 01:09:51 -0300

    feat: configure server to run on specified port from environment variable

--- backend/server.py
+++ backend/server.py
@@ -516,3 +516,7 @@ def add_pix_product():
 # Inicializa o banco de dados
 with app.app_context():
     db.create_all()
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
